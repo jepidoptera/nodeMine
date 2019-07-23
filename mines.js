9c69df96d0a6a29d2f379c2b06d67e352568df38
jepidoptera <jepidoptera@live.com>
2019-07-23 14:43:48 -0500

    added timer, countdown of unflagged mines, and ability to win

--- mines.js
+++ mines.js
@@ -4,10 +4,19 @@ var blessed = require('blessed');
 // always know where the mouse is pointing
 var mousePos = { x: 0, y: 0 };
 
-// track flagged mines, unflagged mines, and flags
+// track flagged mines, unflagged mines, flags, start time, and final time
 var flags = 0;
 var mines = 0;
 var unflaggedMines = 0;
+var startTime = 0;
+var finalTime = 0;
+
+// has the game been won? intially not, but in general, we should know this
+var gameWon = false;
+// the game may also have been lost, and we should know that too
+var gameLost = false;
+// also track whether the game has been started
+var gameStarted = false;
 
 // interval to refresh the screen
 var frameInterval = null;
@@ -17,13 +26,14 @@ var screen = blessed.screen({
     smartCSR: true,
 });
 
-screen.title = 'my window title';
+screen.title = 'NODESWEEPER';
 
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
+// Create a box perfectly centered horizontally and vertically
+// this is where the game resides.
+var mineBox = blessed.box({
     top: 'center',
     left: 'center',
-    width: 33,
+    width: 35,
     height: 18,
     content: '{center}{bold}Nodesweeper!{/bold}!\nclick here to play.{/center}',
     tags: true,
@@ -37,20 +47,30 @@ var box = blessed.box({
     }
 });
 
-// Append our box to the screen.
-screen.append(box);
+// Create a box in the upper left corner for game info
+var infobox = blessed.box({
+    top: 0,
+    left: 0,
+    width: 32,
+    height: 3,
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        bg: 'white',
+        fg: 'black'
+    }
+});
 
-// // Add a png icon to the box
-// var icon = blessed.image({
-//     parent: box,
-//     top: 0,
-//     left: 0,
-//     type: 'overlay',
-//     width: 'shrink',
-//     height: 'shrink',
-//     file: __dirname + '/ball.png',
-//     search: false
-// });
+// Append our boxes to the screen.
+screen.append(mineBox);
+screen.append(infobox);
+
+// Focus our element.
+mineBox.focus();
+// initial screen render
+screen.render();
 
 // generate a map
 var mineMap = {
@@ -59,107 +79,113 @@ var mineMap = {
     cells: []
 };
 
-// the first time the box is clicked, start the game.
-box.on('click', startGame);
+// start the game.
+startGame();
     
 function startGame() {
-    // generate new map
-    mineMap.cells = generateMineMap(16, 16, 40);
+    // reset game variables
+    mines = 40;
+    flags = 0;
+    unflaggedMines = mines;
+    gameWon = false;
+    gameLost = false;
+    gameStarted = false;
+
+    // generate blank map, just so we have something to show
+    mineMap.cells = generateMineMap(16, 16, 0);
 
     // reset click function
-    box.off('click');
-    box.on('click', (event) => {
-        // either clear or flag, depending on the button
-        if (event.button == "left") {
-            clickMine();
-        }
-        else if (event.button == "right") {
-            flagMine();
-        }
+    mineBox.off('click');
+    // next time we click, it will generate the map
+    mineBox.on('click', (event) => {
+
+        // generate new map
+        mineMap.cells = generateMineMap(16, 16, mines);
+
+        // start the game and the timer
+        startTime = Date.now();
+        gameStarted = true;
+
+        // click where they clicked
+        clickMine(mousePos.x, mousePos.y);
+
+        // reset click function (again)
+        mineBox.off('click');
+
+        // now we're actually playing the game
+        mineBox.on('click', (event) => {
+            // either clear or flag, depending on the button
+            if (event.button == "left") {
+                clickMine();
+            }
+            else if (event.button == "right") {
+                flagMine();
+            }
+        });
     });
 
     // set up the render loop @ 33 fps
     if (!frameInterval) frameInterval = setInterval(() => {
+        // display map string
         displayMap(mineMap);
+        // show game info
+        if (!gameStarted)
+            infobox.setContent("40     0:00");
+        else if (!gameWon && !gameLost)
+            // update game timer and remaining mines
+            infobox.setContent((mines - flags) + "     " +
+                (parseInt((Date.now() - startTime) / 1000)) + ":" +
+                (parseInt((Date.now() - startTime) / 10) % 100));
+        else if (gameLost)
+            infobox.setContent((mines - flags) + "  :(");
+        else
+            infobox.setContent("you win! time: " + finalTime);
         // Render the screen.
         screen.render();
     }, 33);
 };
 
 // track mouse
-box.on("mousemove", function (mouse) {
-    let x = mouse.x - box.left - 1;
-    // only change mouse x when they hover of over a cell, not a line
-    if (x % 2 === 0) mousePos.x = x / 2;
-    mousePos.y = mouse.y - box.top - 1;
-    infobox.setContent(`mouse position: ${mousePos.x}, ${mousePos.y}`)
+mineBox.on("mousemove", function (mouse) {
+    let x = mouse.x - mineBox.left - 1;
+    // only change mouse x when they hover over a cell, not a line
+    if (x % 2 !== 0) mousePos.x = x / 2 - 0.5;
+    mousePos.y = mouse.y - mineBox.top - 1;
+    // infobox.setContent(`mouse position: ${mousePos.x}, ${mousePos.y}`)
 });
 
-// Create a box in the upper left corner
-var infobox = blessed.box({
-    top: 0,
-    left: 0,
-    width: 32,
-    height: 3,
-    content: `mouse position: ${mousePos.x}, ${mousePos.y}`,
-    tags: true,
-    border: {
-        type: 'line'
-    },
-    style: {
-        bg: 'white',
-        fg: 'black'
-        // border: {
-        //     fg: '#f0f0f0'
-        // },
-        // hover: {
-        //     bg: 'green'
-        // }
-    }
-});
-
-screen.append(infobox);
-
-// // If box is focused, handle `enter`/`return` and give us some more content.
-box.key('r', function (ch, key) {
-    // regenerate map
-    mineMap.cells = generateMineMap(16, 16, 40);
-});
+// hotkey to reset game (r + s)
+mineBox.key("r", function() {
+    // mineBox.key('s', function (ch, key) {
+        // regenerate map
+    startGame();
+        // });
+})
+// mineBox.removeKey("r", function () {
+//     mineBox.unkey("s");
+// })
 
 // you can also flag with the 'f' key
-box.key('f', () => flagMine());
+// because macbooks don't seem to register a right-click with the terminal
+mineBox.key('f', () => flagMine());
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function (ch, key) {
-    console.log(box.left, box.top);
     return process.exit(0);
 });
 
-// Focus our element.
-box.focus();
-// initial screen render
-screen.render();
-
+// left-clicking on a cell - a risky but necessary move
 function clickMine(x = mousePos.x, y = mousePos.y) {
-    // infobox.width = "100%"; infobox.height = "100%";
-    // infobox.setContent(x);
-    // return;
-    mineMap.cells[x][y].discovered = true;
     if (mineMap.cells[x][y].flagged) {
         // nothing happened when you click on a flagged square
         return;
     }
+    // "discover" this cell
+    mineMap.cells[x][y].discovered = true;
+    // now, what did they click on?
     // did they click on a mine?
-    else if (mineMap.cells[x][y].isMine) {
-        // lose the game, reveal the map
-        for (let x = 0; x < mineMap.width; x++) {
-            for (let y = 0; y < mineMap.height; y++) {
-                mineMap.cells[x][y].discovered = true;
-            }
-        }
-        // next click starts over
-        box.off('click');
-        box.on('click', startGame);
+    if (mineMap.cells[x][y].isMine) {
+        lose();
     }
     // or did they click on a blank spot?
     else if (mineMap.cells[x][y].number === 0) {
@@ -179,7 +205,51 @@ function clickMine(x = mousePos.x, y = mousePos.y) {
 function flagMine(x = mousePos.x, y = mousePos.y) {
     // you can't flag a square which is already revealed
     if (mineMap.cells[x][y].discovered) return;
-    mineMap.cells[x][y].flagged = !mineMap.cells[x][y].flagged;
+    if (!mineMap.cells[x][y].flagged) {
+        // place flag
+        mineMap.cells[x][y].flagged = true;
+        flags += 1;
+        if (mineMap.cells[x][y].isMine) {
+            // successfully flagged a mine. one flag closer to winning
+            unflaggedMines--;
+        }
+    }
+    else {
+        // remove flag
+        mineMap.cells[x][y].flagged = false;
+        flags -= 1;
+        // un-flagged an actual mine.  victory recedes from view
+        unflaggedMines++;
+    }
+    if (unflaggedMines === 0) {
+        // victory condition achieved :)!!!
+        win();
+    }
+}
+
+// win the game
+function win() {
+    gameWon = true;
+    // calculate final time
+    finalTime = (parseInt((Date.now() - startTime) / 1000)) + ":" +
+        (parseInt((Date.now() - startTime) / 10) % 100);
+    // next click starts over
+    mineBox.off('click');
+    mineBox.on('click', startGame);
+}
+
+// lose the game
+function lose() {
+    gameLost = true;
+    // reveal the map
+    for (let x = 0; x < mineMap.width; x++) {
+        for (let y = 0; y < mineMap.height; y++) {
+            mineMap.cells[x][y].discovered = true;
+        }
+    }
+    // next click starts over
+    mineBox.off('click');
+    mineBox.on('click', startGame);
 }
 
 // generating a map of the specified diemensions, 
@@ -197,11 +267,26 @@ function generateMineMap(width, height, mines) {
             let mine = Math.floor(Math.random() * squaresLeft) < minesLeft;
             // never place a mine where the player first clicked, or they would lose immediately
             // which is annoying
-            if (x === mousePos.x && y === mousePos.y) mine = false;
+            if (x === mousePos.x && y === mousePos.y) {
+                mine = false;
+            }
+            // this technically allows a very minor exploit where by clicking on the very last
+            // bottom-right cell, a player can *sometimes* start the game with *one* less mine
+            // but who cares. so forget that, and
             // set "isMine" property
             map[x].push({ isMine: mine });
             // decrement remaining mines
             if (mine) minesLeft--;
+        }
+    }
+    // ok fine, let's make 100% sure there are as many mines as there should be
+    while (minesLeft > 0) {
+        let x = Math.floor(Math.random() * width);
+        let y = Math.floor(Math.random() * height);
+        // and still, we don't want any mines where the player has clicked
+        if (!map[x][y].isMine && (x !== mousePos.x || y !== mousePos.y)) {
+            map[x][y].isMine = true;
+            --minesLeft;
         }
     }
     // count adjacent mines for each cell
@@ -242,6 +327,8 @@ function displayMap(map) {
         // one row per line
         let mapRow = "";
         for (let x = 0; x < map.width; x++) {
+            // inter-column line
+            mapRow += "│";
             // highlight the spot where the mouse is
             if (!map.cells[x][y].discovered) {
                 if (map.cells[x][y].flagged) {
@@ -273,34 +360,33 @@ function displayMap(map) {
             else {
                 // show number of adjacent mines
                 let color = [
+                    // traditional minesweeper colors :)
                     '',
                     'blue',
-                    '#008800',
+                    '#008800', // dark green
                     'red',
-                    '#880088',
-                    '#880000',
-                    '#00bbbb',
+                    '#880088', // purple/dark fuscia
+                    '#880000', // maroon
+                    '#00bbbb', // teal
                     'black',
                     'gray']
-                    // traditional minesweeper colors :)
+                    // choose the one corresponding to the number
                     [map.cells[x][y].number];   
                 // build the display string
                 if (color) {
                     mapRow += `{${color}-fg}${(map.cells[x][y].number).toString()}{/${color}-fg}`;
                 }
                 else {
+                    // gray block for 0 mines
                     mapRow += `{gray-fg}█{/gray-fg}`;
                 }
             }
-            // do a mid-line if this isn't the final column
-            if (x < map.width - 1) {
-                mapRow += "|";
-            }
         }
-        // and finish it with a newline
-        mapString += mapRow + "\n";
+        // and finish it with a vertical line plus a newline
+        mapString += mapRow + "│\n";
     }
     // close off the underline
     mapString += "{/underline}";
-    box.setContent(mapString);
+    // set box content
+    mineBox.setContent(mapString);
 }
